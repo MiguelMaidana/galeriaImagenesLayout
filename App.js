fb645d5b5c9faabc67f0b18965c6f9dad2d27b04
Miguel Maidana <maidana.miguell@gmail.com>
2020-05-04 12:55:44 -0300

    Agregamos un listado de Imagenes , agregamos una cuadricula de imagenes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View,Image, ScrollView } from 'react-native';
 const  App=()=> {
   return (
    <>
-    <View>
+    <ScrollView>
       <View style={{flexDirection:"row"}}>
       <Image
           style={styles.banner}
@@ -46,8 +46,59 @@ const  App=()=> {
           </View>
         </ScrollView>
 
+        <Text style={styles.titulo}>Los mejores Alojamientos</Text>
+        <View>
+          <View>
+                <Image
+                style={styles.mejores}
+                source={require("./assets/img/mejores1.jpg")}
+                />
+            </View>
+            <View>
+                <Image
+                style={styles.mejores}
+                source={require("./assets/img/mejores2.jpg")}
+                />
+            </View>
+            <View>
+                <Image
+                style={styles.mejores}
+                source={require("./assets/img/mejores3.jpg")}
+                />
+            </View>
+        </View>
+        <Text style={styles.titulo}>Hospedajes en LA</Text>
+        <View style={styles.listado}>
+            <View style={styles.listadoItem}>
+                <Image
+                style={styles.mejores}
+                source={require("./assets/img/hospedaje1.jpg")}
+                />
+            </View>
+            <View style={styles.listadoItem}>
+                <Image
+                style={styles.mejores}
+                source={require("./assets/img/hospedaje2.jpg")}
+                />
+            </View>
+            <View style={styles.listadoItem}>
+                <Image
+                style={styles.mejores}
+                source={require("./assets/img/hospedaje3.jpg")}
+                />
+            </View>
+            <View style={styles.listadoItem}>
+                <Image
+                style={styles.mejores}
+                source={require("./assets/img/hospedaje4.jpg")}
+                />
+            </View>
+
+        </View>
+
+
       </View>
-    </View>
+    </ScrollView>
    </>
   );
 }
@@ -70,6 +121,21 @@ const styles = StyleSheet.create({
     width :250,
     height :300,
     marginRight : 10
+  },
+  mejores:{
+    width:"100%",
+    height:200,
+    marginVertical :5
+  },
+  listado:{
+    flexDirection : "row",
+    flexWrap :"wrap",
+    justifyContent : "space-between"
+  },
+  listadoItem:{
+
+    flexBasis : "49%"
+
   }
   
  
